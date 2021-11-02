b3f3b651093f3af28c9d49830a58b7cf0fe56f88
Honneamise <honneamise@hotmail.com>
2021-11-02 13:11:43 +0100

    Update _vs_debug.bat

--- _vs_debug.bat
+++ _vs_debug.bat
@@ -4,5 +4,3 @@ cl.exe /analyze /W3 /MTd /TC DEMO/SRC/fonts.c PIXIE/*.c /link user32.lib gdi32.l
 cl.exe /analyze /W3 /MTd /TC DEMO/SRC/particles.c PIXIE/*.c /link user32.lib gdi32.lib opengl32.lib PIXIE/OPENAL/LIB/OpenAL32.lib /ENTRY:mainCRTStartup /SUBSYSTEM:CONSOLE /MACHINE:X64 /OUT:DEMO/particles.exe 
 cl.exe /analyze /W3 /MTd /TC DEMO/SRC/tones.c PIXIE/*.c /link user32.lib gdi32.lib opengl32.lib PIXIE/OPENAL/LIB/OpenAL32.lib /ENTRY:mainCRTStartup /SUBSYSTEM:CONSOLE /MACHINE:X64 /OUT:DEMO/tones.exe 
 cl.exe /analyze /W3 /MTd /TC DEMO/SRC/sound.c PIXIE/*.c /link user32.lib gdi32.lib opengl32.lib PIXIE/OPENAL/LIB/OpenAL32.lib /ENTRY:mainCRTStartup /SUBSYSTEM:CONSOLE /MACHINE:X64 /OUT:DEMO/sound.exe 
-
-cl.exe /analyze /W3 /MTd /TC DEMO/SRC/test.c PIXIE/*.c /link user32.lib gdi32.lib opengl32.lib PIXIE/OPENAL/LIB/OpenAL32.lib /ENTRY:mainCRTStartup /SUBSYSTEM:CONSOLE /MACHINE:X64 /OUT:DEMO/test.exe 
